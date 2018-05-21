e6fbe03d0018e24dd24a84aaa001ebec5637b97b
Jooyung Han <jooyung.han@gmail.com>
2018-05-22 08:32:48 +0900

    Add separate enum initializer

--- src/com/company/GenEnum.java
+++ src/com/company/GenEnum.java
@@ -11,31 +11,37 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 
+import static org.objectweb.asm.ClassWriter.COMPUTE_MAXS;
 import static org.objectweb.asm.Opcodes.*;
 
 class EnumClass {
   private final String pkg;
   private final String className;
   private final String[] elements;
+  private final String selfName;
+  private final String selfType;
+  private final String selfArrType;
+  private final int INIT_CHUNK = 1000;
 
   public EnumClass(String pkg, String className, String[] elements) {
     this.pkg = pkg;
     this.className = className;
     this.elements = elements;
+    selfName = pkg.replace('.', '/') + "/" + className;
+    selfType = "L" + selfName + ";";
+    selfArrType = "[" + selfType;
   }
 
   public void writeClassFileTo(String classpath) throws IOException {
-    String selfName = pkg.replace('.', '/') + "/" + className;
-    String selfType = "L" + selfName + ";";
-    String selfArrType = "[" + selfType;
-    ClassWriter cw = new ClassWriter(0);
+
+    ClassWriter cw = new ClassWriter(COMPUTE_MAXS);
     cw.visit(V1_5, ACC_PUBLIC + ACC_ENUM + ACC_FINAL + ACC_SUPER, selfName, null, "java/lang/Enum", null);
     for (String e : elements) {
-      cw.visitField(ACC_PUBLIC + ACC_FINAL + ACC_STATIC, e, selfType, null, null).visitEnd();
+      cw.visitField(ACC_PUBLIC + ACC_STATIC, e, selfType, null, null).visitEnd();
     }
 
     {
-      cw.visitField(ACC_PRIVATE + ACC_STATIC + ACC_FINAL + ACC_SYNTHETIC, "$VALUES", selfArrType, null, null).visitEnd();
+      cw.visitField(ACC_STATIC + ACC_FINAL + ACC_SYNTHETIC, "$VALUES", selfArrType, null, null).visitEnd();
     }
 
     {
@@ -61,34 +67,21 @@ class EnumClass {
 
     {
       final MethodVisitor mv = cw.visitMethod(ACC_STATIC, "<clinit>", "()V", null, null);
-      for (int i = 0; i < elements.length; i++) {
-        String e = elements[i];
-        mv.visitTypeInsn(NEW, selfName);
-        mv.visitInsn(DUP);
-        mv.visitLdcInsn(e);
-        visitInt(mv, i);
-        mv.visitMethodInsn(INVOKESPECIAL, selfName, "<init>", "(Ljava/lang/String;I)V", false);
-        mv.visitFieldInsn(PUTSTATIC, selfName, e, selfType);
-      }
 
       visitInt(mv, elements.length);
       mv.visitTypeInsn(ANEWARRAY, selfName);
-      for (int i = 0; i < elements.length; i++) {
-        String e = elements[i];
-        mv.visitInsn(DUP);
-        visitInt(mv, i);
-        mv.visitFieldInsn(GETSTATIC, selfName, e, selfType);
-        mv.visitInsn(AASTORE);
-      }
-
       mv.visitFieldInsn(PUTSTATIC, selfName, "$VALUES", selfArrType);
+      for (int i = 0; i < elements.length; i += INIT_CHUNK) {
+        String initName = writeInitializer(classpath, i);
+        mv.visitMethodInsn(INVOKESTATIC, initName, "init", "()V", false);
+      }
       mv.visitInsn(RETURN);
-      mv.visitMaxs(4, 0);
+      mv.visitMaxs(1, 0);
       mv.visitEnd();
     }
 
     {
-      final MethodVisitor mv = cw.visitMethod(ACC_PRIVATE, "<init>", "(Ljava/lang/String;I)V", null, null);
+      final MethodVisitor mv = cw.visitMethod(0, "<init>", "(Ljava/lang/String;I)V", null, null);
       mv.visitVarInsn(ALOAD, 0);
       mv.visitVarInsn(ALOAD, 1);
       mv.visitVarInsn(ILOAD, 2);
@@ -100,6 +93,41 @@ class EnumClass {
     cw.visitEnd();
     Files.createDirectories(Paths.get(classpath, pkg.replace('.', File.separatorChar)));
     Files.write(Paths.get(classpath, pkg.replace('.', File.separatorChar), className + ".class"), cw.toByteArray(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+  }
+
+  private String writeInitializer(String classpath, int from) throws IOException {
+    final int number = (from / INIT_CHUNK);
+    final String initClassName = className + "$" + number;
+    final String initName = pkg.replace('.', '/') + "/" + className + "$" + number;
+
+    ClassWriter cw = new ClassWriter(COMPUTE_MAXS);
+    cw.visit(V1_5, ACC_FINAL + ACC_SUPER, initName, null, "java/lang/Object", null);
+    {
+      final MethodVisitor mv = cw.visitMethod(ACC_STATIC + ACC_FINAL, "init", "()V", null, null);
+      for (int i = 0; i < INIT_CHUNK && i + from < elements.length; i++) {
+        String e = elements[i + from];
+        mv.visitTypeInsn(NEW, selfName);
+        mv.visitInsn(DUP);
+        mv.visitLdcInsn(e);
+        visitInt(mv, i + from);
+        mv.visitMethodInsn(INVOKESPECIAL, selfName, "<init>", "(Ljava/lang/String;I)V", false);
+        mv.visitFieldInsn(PUTSTATIC, selfName, e, selfType);
+      }
+      for (int i = 0; i < INIT_CHUNK & i + from < elements.length; i++) {
+        String e = elements[i + from];
+        mv.visitFieldInsn(GETSTATIC, selfName, "$VALUES", selfArrType);
+        visitInt(mv, i + from);
+        mv.visitFieldInsn(GETSTATIC, selfName, e, selfType);
+        mv.visitInsn(AASTORE);
+      }
+      mv.visitInsn(RETURN);
+      mv.visitMaxs(4, 0);
+      mv.visitEnd();
+    }
+    cw.visitEnd();
+    Files.write(Paths.get(classpath, pkg.replace('.', File.separatorChar), initClassName + ".class"), cw.toByteArray(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+
+    return initName;
   }
 
   private void visitInt(MethodVisitor mv, int i) {
@@ -123,7 +151,12 @@ class EnumClass {
         mv.visitInsn(ICONST_5);
         break;
       default:
-        mv.visitIntInsn(BIPUSH, i);
+        if (i < 127)
+          mv.visitIntInsn(BIPUSH, i);
+        else if (i < 32767)
+          mv.visitIntInsn(SIPUSH, i);
+        else
+          mv.visitLdcInsn(i);
         break;
     }
   }
@@ -132,7 +165,7 @@ class EnumClass {
 public class GenEnum {
   public static void main(String[] args) throws IOException {
     ArrayList<String> es = new ArrayList<>();
-    for (int i=0; i<3; i++) {
+    for (int i = 0; i < 10000; i++) {
       es.add("VAR" + i);
     }
     new EnumClass("com.x", "Color", es.toArray(new String[0])).writeClassFileTo("classes");
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,31 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println(Arrays.toString(Color.values()));
+    System.out.println(Arrays.toString(X.values()));
+  }
+}
+
+class X {
+  public static X a;
+  public static X b;
+  public static X[] xs;
+
+  public static X[] values() {
+    return xs;
+  }
+
+  static {
+    xs = new X[10000];
+    XInit.init();
+  }
+
+}
+
+class XInit {
+  static void init() {
+    X.a = new X();
+    X.b = new X();
+    X.xs[0] = X.a;
+    X.xs[1] = X.b;
   }
 }
